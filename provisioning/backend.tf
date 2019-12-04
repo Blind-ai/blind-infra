ef4b567537c6bbab38c2df4de3cb3041fee2ea13
Lucas Clerisse <lucasclerisse.pro@gmail.com>
2019-12-04 21:23:05 +0800

    [+](provisioning:backend.tf) Tried to add credentials in terraform backend to avoid New client error

--- provisioning/backend.tf
+++ provisioning/backend.tf
@@ -5,7 +5,7 @@
 terraform {
   backend "gcs" {
     credentials = var.gcp_credentials
-    bucket = "netive-infra-data"
-    prefix = "terraform/state"
+    bucket      = "netive-infra-data"
+    prefix      = "terraform/state"
   }
 }
