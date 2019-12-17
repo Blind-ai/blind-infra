b04325646c93df77a1958063991bb0e2078cc88e
Lucas Clerisse <lucasclerisse.pro@gmail.com>
2019-12-18 00:24:56 +0800

    edit readme

--- provisioning/main.tf
+++ provisioning/main.tf
@@ -2,15 +2,3 @@
   Entry point of Blind IAC
 */
 
-<<<<<<< HEAD
-
-module "netive-network" {
-  source = "./modules/netive-network"
-}
-
-module "netive-platform" {
-  source = "./modules/netive-platform"
-}
-
-=======
->>>>>>> edit readme
